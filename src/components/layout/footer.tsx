6ada7101b61c115919344a3a25c3d07a72de9568
LWai@itiinc.co.jp <LWai@itiinc.co.jp>
2026-07-17 13:00:45 +0900

    change text

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -7,7 +7,7 @@ export function Footer() {
       <div className="container flex flex-col items-center justify-center gap-4 py-6 md:h-20 md:py-0 max-w-full sm:px-4 md:px-8">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
           <p className="text-center text-sm text-muted-foreground md:text-left">
-            wai &copy; {currentYear}. All rights reserved.
+            waimya &copy; {currentYear}. All rights reserved.
           </p>
         </div>
       </div>
